88f429b4ee9aa106b279d0303053594f436b2058
ROUNAK-K-JHA-2002 <rounakjha291202.rar@gmail.com>
2023-06-08 12:15:46 +0530

    complete: Fieldstyle.simple

--- lib/src/text_field.dart
+++ lib/src/text_field.dart
@@ -7,6 +7,8 @@ enum FieldStyle {
   old,
   roundedStyle,
   squareStyle,
+  neumorphic,
+  password,
   custom
 }
 
@@ -14,11 +16,17 @@ class CustomTextField extends StatefulWidget {
   /// Required Parameter:  Controller for text field
   final TextEditingController controller;
 
-  final String? hintText;
-  final Color? borderColor;
-  final Color? boxColor;
-  final Color? textColor;
+  final String hintText;
+  final Color borderColor;
+  final Color boxColor;
+  final Color textColor;
   final FieldStyle fieldStyle;
+  final TextAlign textAlign;
+  final double fontSize;
+  final double letterSpacing;
+  final String fontFamily;
+  final Widget? prefixIcon;
+  final double fieldHeight;
 
   /// An optional maximum number of lines for the text to span, wrapping if necessary.
   /// If the text exceeds the given number of lines, it will be resized according
@@ -35,14 +43,20 @@ class CustomTextField extends StatefulWidget {
   const CustomTextField(
       {super.key,
       required this.controller,
-      this.hintText,
-      this.borderColor,
-      this.boxColor,
-      this.textColor,
-      this.textInputType,
+      this.hintText = "Enter the Value",
+      this.borderColor = Colors.white,
+      this.boxColor = Colors.transparent,
+      this.textColor = Colors.black,
+      this.textInputType = TextInputType.text,
       this.onTap,
-      this.maxLines,
-      this.fieldStyle = FieldStyle.simple});
+      this.maxLines = 1,
+      this.fieldStyle = FieldStyle.simple,
+      this.textAlign = TextAlign.left,
+      this.fontSize = 18,
+      this.letterSpacing = 1,
+      this.fontFamily = '',
+      this.prefixIcon,
+      this.fieldHeight = 50});
 
   @override
   State<CustomTextField> createState() => _CustomTextFieldState();
@@ -52,35 +66,36 @@ class _CustomTextFieldState extends State<CustomTextField> {
   Widget? renderField(BuildContext context) {
     switch (widget.fieldStyle) {
       case FieldStyle.simple:
-        return Container(
-          color: Colors.amber,
+        return SizedBox(
+          height: widget.fieldHeight,
           child: TextFormField(
             onTap: widget.onTap,
             controller: widget.controller,
-            keyboardType: widget.textInputType ?? TextInputType.text,
-            maxLines: widget.maxLines ?? 1,
-            textAlign: TextAlign.center,
+            textAlignVertical: TextAlignVertical.center,
+            keyboardType: widget.textInputType,
+            maxLines: widget.maxLines,
+            textAlign: widget.textAlign,
             style: TextStyle(
-                // fontSize: 17.sp,
+                fontSize: widget.fontSize,
                 color: widget.textColor,
-                fontFamily: 'Montserrat',
-                letterSpacing: 1.1),
+                fontFamily: widget.fontFamily,
+                letterSpacing: widget.letterSpacing),
             decoration: InputDecoration(
-              contentPadding: const EdgeInsets.all(0),
+              contentPadding: const EdgeInsets.symmetric(horizontal: 8),
               enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-                // borderSide: BorderSide(color: widget.borderColor),
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.grey.shade600),
               ),
               focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-                // borderSide: BorderSide(color: widget.borderColor),
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(color: Colors.grey.shade600),
               ),
               filled: true,
               hintText: widget.hintText,
               hintStyle: TextStyle(
                   // fontSize: 16.sp,
                   fontFamily: 'Montserrat',
-                  color: widget.textColor,
+                  color: widget.textColor.withOpacity(0.5),
                   letterSpacing: 1.1),
               fillColor: widget.boxColor,
             ),
@@ -113,7 +128,6 @@ class _CustomTextFieldState extends State<CustomTextField> {
               filled: true,
               hintText: widget.hintText,
               hintStyle: TextStyle(
-                  // fontSize: 16.sp,
                   fontFamily: 'Montserrat',
                   color: widget.textColor,
                   letterSpacing: 1.1),
@@ -262,6 +276,76 @@ class _CustomTextFieldState extends State<CustomTextField> {
           ),
         );
       case FieldStyle.custom:
+        return Container(
+          color: Colors.orange,
+          child: TextFormField(
+            onTap: widget.onTap,
+            controller: widget.controller,
+            keyboardType: widget.textInputType ?? TextInputType.text,
+            maxLines: widget.maxLines ?? 1,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                // fontSize: 17.sp,
+                color: widget.textColor,
+                fontFamily: 'Montserrat',
+                letterSpacing: 1.1),
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(0),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                // borderSide: BorderSide(color: widget.borderColor),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                // borderSide: BorderSide(color: widget.borderColor),
+              ),
+              filled: true,
+              hintText: widget.hintText,
+              hintStyle: TextStyle(
+                  // fontSize: 16.sp,
+                  fontFamily: 'Montserrat',
+                  color: widget.textColor,
+                  letterSpacing: 1.1),
+              fillColor: widget.boxColor,
+            ),
+          ),
+        );
+      case FieldStyle.neumorphic:
+        return Container(
+          color: Colors.orange,
+          child: TextFormField(
+            onTap: widget.onTap,
+            controller: widget.controller,
+            keyboardType: widget.textInputType ?? TextInputType.text,
+            maxLines: widget.maxLines ?? 1,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                // fontSize: 17.sp,
+                color: widget.textColor,
+                fontFamily: 'Montserrat',
+                letterSpacing: 1.1),
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.all(0),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                // borderSide: BorderSide(color: widget.borderColor),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(30),
+                // borderSide: BorderSide(color: widget.borderColor),
+              ),
+              filled: true,
+              hintText: widget.hintText,
+              hintStyle: TextStyle(
+                  // fontSize: 16.sp,
+                  fontFamily: 'Montserrat',
+                  color: widget.textColor,
+                  letterSpacing: 1.1),
+              fillColor: widget.boxColor,
+            ),
+          ),
+        );
+      case FieldStyle.password:
         return Container(
           color: Colors.orange,
           child: TextFormField(
